8a1aeebf05bed5a9bb821a177d860444d15f0b9d
Sean Scofield <seanscofield@github.com>
2025-04-02 18:17:45 +0200

    reset to main

---
+++ build/lib/pianoplayer/__init__.py
@@ -0,0 +1,8 @@
+###########################################################
+__author__ = "Marco Musy"
+__email__ = "marco.musy@gmail.com"
+__version__ = "2.2.0"  # defined in setup.py
+__status__ = "dev"
+__website__ = "https://github.com/marcomusy/pianoplayer"
+__license__ = "MIT"
+
